fe24229c948440e33c7a79b8ef14ecffcd177546
ghghghg2 <sam7793085@gmail.com>
2023-03-11 15:54:27 +0800

    Implement the function to get number of set in cache

--- Lab/lab3_cachelab/cachelab-handout/csim.c
+++ Lab/lab3_cachelab/cachelab-handout/csim.c
@@ -1,7 +1,57 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "getopt.h"
 #include "cachelab.h"
 
-int main()
+typedef unsigned int uint;
+
+#define helpInfo \
+"Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n\
+Options:\n\
+  -h         Print this help message.\n\
+  -v         Optional verbose flag.\
+  -s <num>   Number of set index bits.\n\
+  -E <num>   Number of lines per set.\n\
+  -b <num>   Number of block offset bits.\n\
+  -t <file>  Trace file."
+
+int main(int argc, char *argv[])
 {
+    int opt;
+    uint tmp;
+    uint cacheParam_S;
+    uint cacheParam_E;
+    uint cacheParam_B;
+
+    
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h': /* help */
+                printf(helpInfo);
+            break;
+            case 'v':
+            break;
+            case 's':
+                tmp = strtol(optarg, NULL, 10);
+                if (tmp > 0) {
+                    cacheParam_S = 1 << tmp;
+                }
+            break;
+            case 'E':
+            break;
+            case 'b':
+            break;
+            case 't':
+            break;
+            default:
+            break;
+        }
+    }
+    (void)cacheParam_B;
+    (void)cacheParam_E;
+    (void)cacheParam_S;
+
     printSummary(0, 0, 0);
     return 0;
 }
