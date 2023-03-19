db5d5a24711e2d2a818514c84608025e44cc2f12
Sam Chen <sam7793085@gmail.com>
2023-03-20 00:40:15 +0800

    Define doubly linked list structure

--- Lab/lab3_cachelab/cachelab-handout/csim.h
+++ Lab/lab3_cachelab/cachelab-handout/csim.h
@@ -6,6 +6,63 @@ typedef unsigned char uint8_t;
 typedef char int8_t;
 typedef unsigned long uint64_t;
 
+/* Doubly linked List structure */
+#define listInsertHead(pList, pNewNode) \
+do {\
+    listNode_t *pTmpNode;
+    if((pTmpNode = (pList)->pHeadNode) != NULL) { \
+        pTmpNode->pPrevNode = (pNewNode); \
+    } \
+    (pNewNode)->pNextNode = pTmpNode; \
+    (pNewNode)->pPrevNode = NULL; \
+    (pNewNode)->pList = (pList); \
+} while(0)
+
+#define listMoveNodeToHead(pNode) \
+do { \
+    /* Check if pNode is already head */ \
+    list_t *pList = (pNode)->pList; \
+    listNode_t *pOriginHead; \
+    if ((pOriginHead = pList->pHeadNode) == (pNode)) { \
+        /* Do nothing */ \
+        break; \
+    } \
+    /* Connect the prev node to next node */ \
+    listNode_t *pTmpPrevNode = (pNode)->pPrevNode; \
+    listNode_t *pTmpNextNode = (pNode)->pNextNode; \
+    pTmpPrevNode->pNextNode = pTmpNextNode; \
+    pTmpNextNode->pPrevNode = pTmpPrevNode; \
+    (pNode)->pNextNode = pOriginHead; \
+    (pNode)->pPrevNode = NULL; \
+    pOriginHead->pPrevNode = (pNode); \
+    pList->pHeadNode = (pNode); \
+} while(0)
+
+#define getNextNode(pNode) ((pNode)->pNextNode)
+#define getPrevNode(pNode) ((pNode)->pPrevNode)
+#define getOwnerPtr(pNode, ownerType_t, nodeMemberName) \
+((ownerType_t *)((void *)(pNode) - (void *)(&(ownerType_t*)(0)->nodeMemberName)))
+
+typedef struct listNode_t{
+    struct listNode_t *pPrevNode;
+    struct listNode_t *pNextNode;
+    struct void *pList;
+}listNode_t;
+
+typedef struct {
+    listNode_t *pHeadNode;
+}list_t;
+
+
+/* Cache structure */
+
+typedef struct {
+    bool valid;
+    uint64_t tagVal;
+    listNode_t lineNode;
+}cacheLine_t;
+
+
 
 typedef enum {
     INSTRUCTION = 'I', 
