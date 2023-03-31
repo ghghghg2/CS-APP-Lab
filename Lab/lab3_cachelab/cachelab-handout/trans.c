eb83600325599547990ea42195ec1ca610295a4a
ghghghg2 <sam7793085@gmail.com>
2023-03-31 17:58:37 +0800

    Try registering function

--- Lab/lab3_cachelab/cachelab-handout/trans.c
+++ Lab/lab3_cachelab/cachelab-handout/trans.c
@@ -22,6 +22,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
 }
 
 /* 
@@ -34,6 +43,20 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
  */
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
+}
+
+char my_trans_desc[] = "my transpose";
+void my_trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
@@ -60,6 +83,7 @@ void registerFunctions()
 
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
+    registerTransFunction(my_trans, my_trans_desc); 
 
 }
 
